8383357227a8e4d21e9f042a188824b1720b34db
Mahesh Kakde <maheshkakde100@gmail.com>
2022-06-30 23:40:53 +0530

    Day 30 Muimum moves require to array size II

---
+++ 06_June_2022_LeetCode_Challange/Day_30_Minimum_moves_require_to_size_II.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0, median = nums[nums.length / 2];
+        for (int num : nums) ans += Math.abs(median - num);
+        return ans;
+    }
+}
